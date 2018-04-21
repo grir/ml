de8ea65a92ed9ff30d3aadede8f7cff939fbc884
k420 <irus.grinis@gmail.com>
2018-04-21 20:29:23 +0300

    new file:   2.R 	generators.cpp 	nnetEx.R 	out2.dat

---
+++ 2.R
@@ -0,0 +1,14 @@
+# Rcpp::sourceCpp('generators.cpp')
+# means = matrix(c(0,0,2,2),nrow=2, byrow = TRUE)
+# sds = matrix(c(1,1,1,1),nrow=2, byrow = TRUE)
+# mats = generateStdNormalMatrix(means, sds, seed = 123, numRows = 10)
+
+library(MASS)
+
+
+mvStdNorms <- function(nrow=1,means=c(0,0), sds=c(1,1), clsId=1){
+  sigma <- diag(sds)  
+  result <- mvrnorm(nrow, means, sigma)
+  result <- cbind(result,rep(clsId,nrow)) 
+  return(result)
+}
